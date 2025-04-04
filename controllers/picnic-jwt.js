c97591084b59132399c98e03a0246559a0ddfc28
mehranbinmoin <mehranmoin@gmail.com>
2025-04-03 22:28:15 -0400

    Verified token is valid via Postman.

--- controllers/picnic-jwt.js
+++ controllers/picnic-jwt.js
@@ -1,13 +1,21 @@
 const express = require('express')
 const router = express.Router()
+const jwt = require('jsonwebtoken')
 
 router.get('/sign-token', (req, res) => {
     const user = {
-        _id: 42,
+        _id: 25,
         username: 'Mehran',
         password: '1234'
     }
-    res.json({ message: 'You are authorized!' })
+
+    const token = jwt.sign({ user }, process.env.JWT_SECRET)
+
+    res.json({ token })
+})
+
+router.post('/verify-token', (req, res) => {
+    res.json({ message: 'Your token is valid!' })
 })
 
 module.exports = router
