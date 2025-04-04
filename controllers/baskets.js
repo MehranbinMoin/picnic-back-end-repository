8529d7159b2cdeed509abb21a936b13d7c7a5294
mehranbinmoin <mehranmoin@gmail.com>
2025-04-04 16:06:01 -0400

    Finished creating the basket create CRUD operation in the baskets dot js file.

--- controllers/baskets.js
+++ controllers/baskets.js
@@ -3,6 +3,15 @@ const verifyToken = require('../middleware/verify-token')
 const Basket = require('../models/basket')
 const router = express.Router()
 
-
+router.post('/', verifyToken, async (req, res) => {
+    try {
+        req.body.author = req.user._id
+        const basket = await Basket.create(req.body)
+        basket._doc.author = req.user
+        res.status(201).json(basket)
+    } catch (error) {
+        res.status(500).json({ err: error.message })
+    }
+})
 
 module.exports = router
