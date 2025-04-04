56d3ba0c810134785e6dfc1bc1b6cc909090cf16
mehranbinmoin <mehranmoin@gmail.com>
2025-04-04 01:17:10 -0400

    Adding router handler on users dot js file to prevent users from accessing information regarding other user accounts.

--- controllers/users.js
+++ controllers/users.js
@@ -13,4 +13,16 @@ router.get('/', verifyToken, async (req, res) => {
     }
 })
 
+router.get('/:userId', verifyToken, async (req, res) => {
+    try {
+        const user = await User.findById(req.params.userId)
+        if (!user) {
+            return res.status(404).json({ err: 'User is not found.'})
+        }
+        res.json(user)
+    } catch (err) {
+        res.status(500).json({ err: err.message })
+    }
+})
+
 module.exports = router
