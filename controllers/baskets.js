486e9ac1839038c44d039c2386cd916262f4fa7d
mehranbinmoin <mehranmoin@gmail.com>
2025-04-04 16:58:32 -0400

    Created update basket router handler in baskets dot js file and tested on Postman to  make sure the update handler works.

--- controllers/baskets.js
+++ controllers/baskets.js
@@ -34,4 +34,18 @@ router.get('/:basketId', verifyToken, async (req, res) => {
     }
 })
 
+router.put('/:basketId', verifyToken, async (req, res) => {
+    try {
+        const basket = await Basket.findById(req.params.basketId)
+        if (!basket.author.equals(req.user._id)) {
+            return res.status(403).json({ err: 'You are not the creator of this basket!' })
+        }
+        const updatedBasket = await Basket.findByIdAndUpdate(req.params.basketId, req.body, { new: true})
+        updatedBasket._doc.author = req.user
+        res.status(200).json(updatedBasket)
+    } catch (error) {
+        res.status(500).json({ err: error.message })
+    }
+})
+
 module.exports = router
