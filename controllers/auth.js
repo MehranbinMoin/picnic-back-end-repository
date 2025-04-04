4b9291b08f6a815b1e8d724ba0fb85ba8c54572d
mehranbinmoin <mehranmoin@gmail.com>
2025-04-03 23:42:27 -0400

    Created user model.

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const router = express.Router()
+const bcrypt = require('bcrypt')
+const User = require('../models/user')
 
 router.post('/sign-up', (req, res) => {
     res.json({ message: 'Sign up complete!' })
