7c6807627b2756a0f7b548ef424e72d7e71e530e
mehranbinmoin <mehranmoin@gmail.com>
2025-04-04 00:47:48 -0400

    Creating middleware to verify token in middleware on verify token dot js file.

---
+++ middleware/verify-token.js
@@ -0,0 +1,13 @@
+const jwt = require('jsonwebtoken')
+
+const verifyToken = (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1]
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        res.json({ decoded })
+    } catch (err) {
+        res.status(401).json({ err: 'Invalid token' })
+    }
+}
+
+module.exports = verifyToken
