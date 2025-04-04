e9a58b4b572679ccb097188e17ac8e38d0c004e1
mehranbinmoin <mehranmoin@gmail.com>
2025-04-03 23:48:59 -0400

    Writing try catch block to prevent a user from creating an account with a username that is already taken.

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,8 +3,16 @@ const router = express.Router()
 const bcrypt = require('bcrypt')
 const User = require('../models/user')
 
-router.post('/sign-up', (req, res) => {
-    res.json({ message: 'Sign up complete!' })
+router.post('/sign-up', async (req, res) => {
+    try {
+        const userInDatabase = await User.findOne({ username: req.body.username })
+
+        if (userInDatabase) {
+            return res.status(409).json({ err: 'Username is already taken.'})
+        }
+    } catch (err) {
+
+    }
 })
 
 module.exports = router
