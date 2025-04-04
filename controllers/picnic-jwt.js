18fba404ea6bea14ba9b8067be224e1a77497e48
mehranbinmoin <mehranmoin@gmail.com>
2025-04-03 22:18:53 -0400

    Generated secret key for env file using crypto + randomBytes.

--- controllers/picnic-jwt.js
+++ controllers/picnic-jwt.js
@@ -2,6 +2,11 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/sign-token', (req, res) => {
+    const user = {
+        _id: 42,
+        username: 'Mehran',
+        password: '1234'
+    }
     res.json({ message: 'You are authorized!' })
 })
 
